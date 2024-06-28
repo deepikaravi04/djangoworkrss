3390862a5d9580732972e9d29d8bb711145a0964
Akil Akil <deepikaraviup@gmail.com>
2024-06-28 16:41:05 +0530

    Admin file updated

--- feedviewer/admin.py
+++ feedviewer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import RSSFeedLink, JobFilterTerm, JobListing
 # Register your models here.
+admin.site.register(RSSFeedLink)
+admin.site.register(JobFilterTerm)
+admin.site.register(JobListing)
